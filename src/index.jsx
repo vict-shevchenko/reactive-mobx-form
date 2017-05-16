79c4acc45778952ab095a111775976deb4750b69
viktor.shevchenko <viktor@geeny.io>
2017-05-16 18:28:17 +0300

    Start point for building a documentation web site

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,3 @@
-import React from 'react';
-import { Provider } from 'mobx-react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import AppState from './AppState';
-import App from './App';
+let a = 10;
 
-import { FormStore } from 'reactive-mobx-form';
-
-const appState = new AppState();
-const formStore = new FormStore();
-
-render(
-  <AppContainer>
-      <Provider appState={appState} formStore={formStore}>
-          <App />
-      </Provider>
-  </AppContainer>,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default;
-
-    render(
-      <AppContainer>
-          <Provider appState={appState} formStore={formStore}>
-              <App />
-          </Provider>
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
-}
+console.log(a);
