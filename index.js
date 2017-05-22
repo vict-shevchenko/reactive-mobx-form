2fe434cdec5f258f5e93b69b5f6cc5147555b855
viktor.shevchenko <viktor@geeny.io>
2017-05-22 23:06:57 +0300

    initial commin to gh-pages

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-module.exports.default = module.exports;
-
-exports.reactiveMobxForm = require('./lib/createForm').reactiveMobxForm;
-exports.FormStore        = require('./lib/Store').FormStore;
-exports.Control            = require('./lib/ui/Control').Control;
