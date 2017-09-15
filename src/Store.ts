3aadf9f51d99c9aa04c6bbee77f7fc0101ada38d
viktor.shevchenko <viktor@geeny.io>
2017-09-15 12:30:31 +0300

    TSLinted Store.ts

--- src/Store.ts
+++ src/Store.ts
@@ -2,15 +2,15 @@ import { observable, ObservableMap } from 'mobx';
 import { Form } from './Form';
 
 export class FormStore {
-	@observable forms: ObservableMap<{}> = observable.map();
+	@observable public forms: ObservableMap<Form> = observable.map();
 
-	registerForm(name: string, form:Form) {
+	public registerForm(name: string, form: Form): void {
 		this.forms.set(name, form);
 	}
 
-	unRegisterForm(name:string){
+	public unRegisterForm(name: string): void {
 		if (this.forms.has(name)) {
-			this.forms.delete(name)
+			this.forms.delete(name);
 		}
 	}
 }
