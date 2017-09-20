be5795202ff187d012bb98196bd9d891193bece0
viktor.shevchenko <viktor@geeny.io>
2017-09-20 18:24:33 +0300

    Unit tests for FormStore

--- __tests__/Store.test.ts
+++ __tests__/Store.test.ts
@@ -1,10 +1,29 @@
-declare var jest, describe, it, expect;
+declare var jest, describe, it, expect, beforeAll;
 import { FormStore} from '../src/Store';
+import { Form } from '../src/Form';
 
 describe('Testing the FormStore', () => {
-	test('Creation on new FormStore instance should wokr correctly', () => {
-		// const formStore = new FormStore();
+	let formStore: FormStore;
 
-		// expect(10).toEqual(expect.any(Number));
+	beforeAll(() => {
+		formStore = new FormStore();
+	});
+
+	test('Creation on new FormStore instance should wokr correctly', () => {
+		expect(formStore).toEqual(expect.any(FormStore));
+	});
+
+	test('created instance of FormStore should have proper methods and properties', () => {
+		expect(formStore.forms).toBeDefined();
+		expect(formStore.forms.size).toBe(0);
+
+		formStore.registerForm('myform', new Form({}, {}, {}));
+
+		expect(formStore.forms.get('myform')).toBeDefined();
+
+		formStore.unRegisterForm('myform');
+
+		expect(formStore.forms.get('myform')).not.toBeDefined();
+		expect(formStore.forms.size).toBe(0);
 	});
 });
