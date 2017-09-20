5cb2395b65561d0994ba7bc9fe2ef4b06904dee9
viktor.shevchenko <viktor@geeny.io>
2017-09-20 18:04:48 +0300

    Preparing tests for Store

---
+++ __tests__/Store.test.ts
@@ -0,0 +1,10 @@
+declare var jest, describe, it, expect;
+import { FormStore} from '../src/Store';
+
+describe('Testing the FormStore', () => {
+	test('Creation on new FormStore instance should wokr correctly', () => {
+		// const formStore = new FormStore();
+
+		// expect(10).toEqual(expect.any(Number));
+	});
+});
